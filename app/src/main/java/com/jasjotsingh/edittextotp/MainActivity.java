c9cd8a2b092f020f3bd9e35f4ba27995ce460634
Jasjot Singh <jasjot784@gmail.com>
2020-08-12 13:55:23 +0530

    Added Required Code

--- app/src/main/java/com/jasjotsingh/edittextotp/MainActivity.java
+++ app/src/main/java/com/jasjotsingh/edittextotp/MainActivity.java
@@ -2,13 +2,82 @@ package com.jasjotsingh.edittextotp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText et1,et2,et3,et4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et1 = findViewById(R.id.et1);
+        et2 = findViewById(R.id.et2);
+        et3 = findViewById(R.id.et3);
+        et4 = findViewById(R.id.et4);
+
+        et1.addTextChangedListener(new GenericTextWatcher(et1));
+        et2.addTextChangedListener(new GenericTextWatcher(et2));
+        et3.addTextChangedListener(new GenericTextWatcher(et3));
+        et4.addTextChangedListener(new GenericTextWatcher(et4));
     }
+
+    public class GenericTextWatcher implements TextWatcher
+    {
+        private View view;
+        private GenericTextWatcher(View view)
+        {
+            this.view = view;
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            // TODO Auto-generated method stub
+            String text = editable.toString();
+            switch(view.getId())
+            {
+
+                case R.id.et1:
+                    if(text.length()==1)
+                        et2.requestFocus();
+                    break;
+                case R.id.et2:
+                    if(text.length()==1)
+                        et3.requestFocus();
+                    else if(text.length()==0)
+                        et1.requestFocus();
+                    break;
+                case R.id.et3:
+                    if(text.length()==1)
+                        et4.requestFocus();
+                    else if(text.length()==0)
+                        et2.requestFocus();
+                    break;
+                case R.id.et4:
+                    if(text.length()==0)
+                        et3.requestFocus();
+                    else if(text.length()==1){
+                        Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                        startActivity(intent);
+                    }
+                    break;
+            }
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+            // TODO Auto-generated method stub
+        }
+    }
+
 }
